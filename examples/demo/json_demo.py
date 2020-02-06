4eacb5971b6482c98f52dde9dd1a0c2973ce878b
Daniel Bakkelund <daniel_bakkelund@hotmail.com>
2020-02-06 20:12:49 +0100

    Added copyright and license notices everywhere

--- examples/demo/json_demo.py
+++ examples/demo/json_demo.py
@@ -4,6 +4,23 @@ import numpy              as np
 
 import ophac.hac          as hac
 import ophac.json_support as jsn
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Copyright 2020 Daniel Bakkelund
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
 import ophac.dendrogram   as dnd
 
 inputFname='examples/demo/input.json'
