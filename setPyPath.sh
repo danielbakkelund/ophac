1aa3d9cb10dc60a8adc921842be5deb5b61d42be
Daniel Bakkelund <daniel_bakkelund@hotmail.com>
2020-03-19 12:25:35 +0100

    robustified

--- setPyPath.sh
+++ setPyPath.sh
@@ -24,32 +24,39 @@ else
     CYGW=1
 fi
 
-# Relative path to unit test framework.
-UPYT_HOME=${PWD}'/xlibs/upyt'
+if test -e ${PWD}'/xlibs/upyt' ; then
 
-# upyt dir
-UPYT_ABS_SRC="unset"
-cd $UPYT_HOME
-if test "$CYGW" -eq "0"; then
-    UPYT_ABS_SRC=`cygpath -a -w $PWD/src`
-    UPYT_ABS_SRC=${UPYT_ABS_SRC//\\/\\\\}
-else
-    UPYT_ABS_SRC=${PWD}/src
+    # Relative path to unit test framework.
+    UPYT_HOME=${PWD}'/xlibs/upyt'
+
+    # upyt dir
+    UPYT_ABS_SRC="unset"
+    cd $UPYT_HOME
+    if test "$CYGW" -eq "0"; then
+	UPYT_ABS_SRC=`cygpath -a -w $PWD/src`
+	UPYT_ABS_SRC=${UPYT_ABS_SRC//\\/\\\\}
+    else
+	UPYT_ABS_SRC=${PWD}/src
+    fi
+    cd - > /dev/null
+
+
+    # Current dir
+    if test "$CYGW" -eq "0"; then
+	P=`cygpath -a -w $PWD/src`
+	PP=${P//\\/\\\\}
+	T=`cygpath -a -w $PWD/test`
+	TP=${T//\\/\\\\}
+	T=`cygpath -a -w $PWD/systest`
+	TP=${T//\\/\\\\}
+	export PYTHONPATH=${PP}';'${TP}';'${UPYT_ABS_SRC}
+    else
+	export PYTHONPATH=${PWD}/src':'${PWD}/test':'${UPYT_ABS_SRC}':'${PWD}/systest
+    fi
+
+    echo "PYTHONPATH=$PYTHONPATH"
+
+else # not exists xlibs
+    echo '##################### ERROR #########################'
+    echo 'You must run ./init.sh prior to sourcing this script.'
 fi
-cd - > /dev/null
-
-
-# Current dir
-if test "$CYGW" -eq "0"; then
-    P=`cygpath -a -w $PWD/src`
-    PP=${P//\\/\\\\}
-    T=`cygpath -a -w $PWD/test`
-    TP=${T//\\/\\\\}
-    T=`cygpath -a -w $PWD/systest`
-    TP=${T//\\/\\\\}
-    export PYTHONPATH=${PP}';'${TP}';'${UPYT_ABS_SRC}
-else
-    export PYTHONPATH=${PWD}/src':'${PWD}/test':'${UPYT_ABS_SRC}':'${PWD}/systest
-fi
-
-echo "PYTHONPATH=$PYTHONPATH"
