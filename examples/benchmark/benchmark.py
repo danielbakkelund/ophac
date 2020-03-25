d4ffbd8929e25137f4a883916c61bc32f1a65cf8
Daniel Bakkelund <daniel_bakkelund@hotmail.com>
2020-03-25 14:55:59 +0100

    adjustment

--- examples/benchmark/benchmark.py
+++ examples/benchmark/benchmark.py
@@ -7,23 +7,31 @@ with open('examples/benchmark/M.dat', 'r') as inf:
 with open('examples/benchmark/Q.dat', 'r') as inf:
     Q = eval(inf.readline())
 
-L = ['single', 'average', 'complete']
-T = []
-for lnk in L:
+L      = ['date', 'single', 'average', 'complete']
+T      = [str(datetime.date.today())]
+format = lambda x : '%1.0f' % x
+for lnk in L[1:]:
     print(lnk + ' linkage...')
     hc = hac.HAC(lnk)
     start = time.time()
     hc.generate(M,Q)
     duration = time.time() - start
     print('duration: %1.0f s.' % duration)
-    T.append(duration)
+    T.append(format(duration))
 
-frm  = lambda x : '%1.0f' % x
-date = str(datetime.date.today())
+mlen = 0    
+with open('examples/benchmark/log.csv', 'r') as inf:
+    line = inf.readline()
+    while line != '':
+        print(line.strip().replace(',','\t'))
+        mlen = max(mlen, len(line))
+        line = inf.readline()
+                
+print('-'*mlen)
+print('\t'.join(T))
 
-L.insert(0, 'date')
-T = map(frm, T)
-T.insert(0, date)
-print('summary:')
-print(','.join(L))
-print(','.join(T))
+print('\nAppend to log [y/n] ?')
+if raw_input() in ['y','Y']:
+    with open('examples/benchmark/log.csv', 'a') as outf:
+        outf.write(','.join(T) + '\n')
+
