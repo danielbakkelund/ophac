640183cfb046d6f739a7e9dbbb523ae1da6103da
Daniel Bakkelund <daniel_bakkelund@hotmail.com>
2020-03-25 19:43:31 +0100

    p2+p3

--- examples/benchmark/benchmark.py
+++ examples/benchmark/benchmark.py
@@ -3,6 +3,12 @@ import time
 import datetime
 import sys
 
+def getInput(prompt='>'):
+    try:
+        return raw_input(prompt)
+    except NameError as e:
+        return input(prompt)
+
 with open('examples/benchmark/M.dat', 'r') as inf:
     M = eval(inf.readline())
 with open('examples/benchmark/Q.dat', 'r') as inf:
@@ -32,9 +38,9 @@ print('-'*(mlen+15)) # Compensate for tab stops
 print('\t'.join(T))
 
 print('\nAppend to log [y/n] ?')
-if raw_input() in ['y','Y']:
+if getInput() in ['y','Y']:
     print('Please provide a comment:')
-    cmt = raw_input('>')
+    cmt = getInput('>')
     T.append(cmt)
     with open('examples/benchmark/log.csv', 'a') as outf:
         outf.write(','.join(T) + '\n')
