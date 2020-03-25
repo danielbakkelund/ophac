53b38c4979033f21da11c4034f25f76117dc2385
Daniel Bakkelund <daniel_bakkelund@hotmail.com>
2020-03-25 15:11:30 +0100

    Updated to larger data set

--- examples/benchmark/benchmark.py
+++ examples/benchmark/benchmark.py
@@ -1,6 +1,7 @@
 import ophac.hac as hac
 import time
 import datetime
+import sys
 
 with open('examples/benchmark/M.dat', 'r') as inf:
     M = eval(inf.readline())
@@ -9,15 +10,15 @@ with open('examples/benchmark/Q.dat', 'r') as inf:
 
 L      = ['date', 'single', 'average', 'complete']
 T      = [str(datetime.date.today())]
-format = lambda x : '%1.0f' % x
+format = lambda x : '%1.1f' % x
 for lnk in L[1:]:
     print(lnk + ' linkage...')
     hc = hac.HAC(lnk)
     start = time.time()
     hc.generate(M,Q)
     duration = time.time() - start
-    print('duration: %1.0f s.' % duration)
     T.append(format(duration))
+    print('duration: %s s.' % T[-1])
 
 mlen = 0    
 with open('examples/benchmark/log.csv', 'r') as inf:
@@ -27,11 +28,14 @@ with open('examples/benchmark/log.csv', 'r') as inf:
         mlen = max(mlen, len(line))
         line = inf.readline()
                 
-print('-'*mlen)
+print('-'*(mlen+15)) # Compensate for tab stops
 print('\t'.join(T))
 
 print('\nAppend to log [y/n] ?')
 if raw_input() in ['y','Y']:
+    print('Please provide a comment:')
+    cmt = raw_input('>')
+    T.append(cmt)
     with open('examples/benchmark/log.csv', 'a') as outf:
         outf.write(','.join(T) + '\n')
 
