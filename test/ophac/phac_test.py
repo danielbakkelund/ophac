8562a405477ea90c4af794f03a6ad77d7ab8b50d
Daniel Bakkelund <daniel_bakkelund@hotmail.com>
2020-05-11 16:21:06 +0200

    starting to develop p-model

--- test/ophac/phac_test.py
+++ test/ophac/phac_test.py
@@ -13,3 +13,15 @@ class PlustTransformTest(ut.UnitTest):
         self.assertEquals([(2,3),(3,4)],phac._T_inv(i,j,2,3))
         self.assertEquals([(3,5)],phac._T_inv(i,j,3,4))
         self.assertEquals([(5,6)],phac._T_inv(i,j,4,5))
+
+    def test_transform(self):
+        P0 = phac.DistMatrix([0.1, 0.2, 0.3,
+                              0.2, 0.1,
+                              0.1])
+        i,j = 0,3
+        P1  = phac.plusTransformation(P0,i,j)
+
+        expected = phac.DistMatrix([2/7, 3/7,
+                                    2/7])
+
+        self.assertEquals(expected, P1)
