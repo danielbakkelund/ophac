bae8369062163f1ea7770afb59600628d2846bd3
Daniel Bakkelund <daniel_bakkelund@hotmail.com>
2020-05-12 13:09:00 +0200

    removed order support from phac, since it is not clear how to cost-function partial p-dendrograms.

--- src/ophac/phac.py
+++ src/ophac/phac.py
@@ -17,7 +17,7 @@
 
 from   ophac.dtypes import *
 import ophac.ultrametric as ult
-
+import numpy as np
 
 def _getLogger(x):
     import logging
@@ -56,168 +56,110 @@ def _plusTransformation(P0,i,j):
                 P1[a,b] = P1[a,b] + P0[x,y]/C
 
     return P1
-                
-def _pUltrametric(ac,N):
-    Md = 0
-    if len(ac) > 0:
-        Md = ac.dists[-1] - 1e-12
-    dK = 1 - Md
-    return ult.ultrametric(ac, N, dK)
 
+def _pAC(ac):
+    if len(ac) == 0:
+        return ac
+    mp = [0]*len(ac)
+    mp[0] = ac.dists[0]
+    for i in range(1,len(ac)):
+        mp[i] = mp[i-1]*ac.dists[i]
+
+    ip = [1-p for p in mp]
+    return AC(joins=ac.joins, dists=ip)
+
+def _acProb(ac):
+    pac = _pAC(ac)
+    return np.product(ac.dists)
+
+def _pUltrametric(ac,N):
+    ac2 = _pAC(ac)
+    dK  = 1
+    if len(ac2) > 0:
+        dK  = 1 - ac2.dists[-1]
+    return ult.ultrametric(ac2,N,dK)
+    
+    
 class PHAC:
 
-    def __init__(self, trans, ord=1, dK=1e-12, pred=True, cutoff=True):
+    def __init__(self, trans):
         '''
         trans  - Probability transformation - one of '+'.
-        ord    - The norm-order to use. Default is 1.0.
-        dK     - The increment to apply for the ultrametric completion.
-        pred   - Use partial order reduction? Default is True.
-        cutoff - Use fit detorieration cutoff. Defaut it True
         '''
         self.log          = _getLogger(PHAC)
         self.trans        = trans
-        self.ord          = ord
-        self.dK           = dK
-        self.pred         = pred
-        self.cutoff       = cutoff
         self._setTransformation()
-        self.log.info('Instantiated with transform=%s and ord=%1.3f.', trans, ord)
+        self.log.info('Instantiated with transform=%s.', trans)
 
     def _setTransformation(self):
         assert self.trans == '+'
         self._transform = _plusTransformation
         
-    def _initClustering(self, P0, quivers):
-        assert len(quivers) == P0.n
+    def _initClustering(self,P0):
+        self.N        = P0.n
+        self.bestProb = 0.0  # Highest probability so far
+        self.acs      = []   # Best chains
 
-        self.N  = len(quivers)
-        self.P0 = P0
-
-        ac0  = AgglomerativeClustering()
-        U0   = _pUltrametric(ac0, self.N)
-        norm = U0.norm(self.ord)
-
-        self.bestNorm = norm       # Closest ultrametric
-        self.acs      = [ac0]      # Maximal best chains
-        self.ults     = set([U0])  # Visited ultrametrics
-
-    def generate(self,probs,order=None):
+    def generate(self,probs):
         '''
         probs  - Probabilities for pair agglomerations
-        order  - Order relation of type ophac.dtypes.Quivers, 
-                 or None for non-ordered clustering.
         '''
-        if order is None:
-            order = Quivers(n=probs.n, relation=[])
-
-        self._initClustering(probs, order)
-        
-        self._exploreChains(probs, order, self.acs[-1], self.bestNorm)
-
+        self._initClustering(probs)
+        self._exploreChains(probs, AC())
         return self._pickBest()
         
-    def _exploreChains(self, P1, order, ac0, lastDiff):
+    def _exploreChains(self, P1, ac1):
         '''
         P1        - Probabilities for merging blocks
-        order     - Quivers object for the current partition
         ac0       - AgglomerativeClustering object leading up to the 
                     current partition
         '''
-        self.log.debug('Exploring %d block partition.', len(order))
+        self.log.debug('Exploring %d block partition.', P1.n)
 
-        if len(order) == 1:
+        if P1.n == 1:
             self.log.debug('Trivial partition reached.')
-            self._registerCandidate(ac0)
-            return 
-    
-        # Find maximal probability to merge on
-        chunks = P1.getChunkedIndexPairs()
-        chunk  = None
-        for _ch in chunks:
-            if chunk is not None:
-                break
-            for a,b in _ch.pairs:
-                if order.canMerge(a,b):
-                    self.log.debug('Mergeable pair (%d,%d) of p=%1.3f found.',
-                                   a, b, _ch.dist)
-                    chunk = _ch
-                    break
-
-        if chunk is None:
-            self.log.debug('No further mergeable pairs --- maximal element reached')
-            self._registerCandidate(ac0)
+            self._registerCandidate(ac1)
             return 
 
-        merged = False
-        for a,b in chunk.pairs:
-            if order.canMerge(a,b):
-                self.log.debug('Merging (%d,%d) at probability %1.3f.',
-                               a, b, chunk.dist)
-                P2  = self._transform(P1,a,b)
-                O2  = order.merge(a,b)
-                ac2 = ac0 + AC(joins=[(a,b)], dists=[chunk.dist])
-                merged = True
-                if len(ac2) > 1:
-                    assert ac2.dists[-2] <= ac2.dists[-1]
-                diff, better = self._checkVisit(ac2, lastDiff)
-                if better:
-                    self._exploreChains(P2, O2, ac2, diff)
-
-        assert merged
+        P     = np.array(P1.dists)
+        p     = np.max(P)
+        I     = np.argwhere(P == p).flatten()
+        pairs = [P1.toMatrixIndex(i) for i in I]
+        
+        for a,b in pairs:
+            self.log.debug('Merging (%d,%d) at probability %1.3f.',
+                           a, b, p)
+            P2  = self._transform(P1,a,b)
+            ac2 = ac1 + AC(joins=[(a,b)], dists=[p])
+            self._exploreChains(P2, ac2)
 
         return
-
-    def _checkVisit(self, ac, lastDiff):
-        '''
-        Returns true if this chain should be attempted extended.
-        '''
-        U  = _pUltrametric(ac, self.N)
-        
-        if self.pred:
-            l0 = len(self.ults)
-            self.ults.add(U)
-            l1 = len(self.ults)
-            if l0 == l1:
-                # Already visited this state
-                return (None,False)
-
-        if not self.cutoff:
-            return (0,True)
-        
-        diff   = U.norm(self.ord)
-        better = diff <= lastDiff or diff <= self.bestNorm
-        return (diff, better)
-
+    
     def _registerCandidate(self, ac):
-        U    = _pUltrametric(ac, self.N)
-        norm = U.norm(self.ord)
-
-        if norm > self.bestNorm:
+        assert len(ac) == self.N-1
+        prob = _acProb(ac)
+        
+        if prob < self.bestProb:
             # discard candidate
             return
 
-        if norm < self.bestNorm:
-            self.log.info('New best candidate at norm %1.3e', norm)
-            self.acs      = [ac]
-            self.bestNorm = norm
+        if prob > self.bestProb:
+            self.log.info('New best candidate at probability %1.3e', prob)
+            self.acs      = [_pAC(ac)]
+            self.bestProb = prob
         else:
-            # Same norm
-            self.acs.append(ac)
+            # Same probability
+            self.acs.append(_pAC(ac))
 
     def _pickBest(self):
-        import numpy as np
         self.log.info('Finding best of %d dendrograms.', len(self.acs))
 
         class _MySet(set):
             def add(self,x):
-                n0 = len(self)
+                n = len(self)
                 set.add(self,x)
-                return len(self) > n0
+                return len(self) > n
         
-        if len(self.acs) == 1:
-            self.log.info('Returning unique solution.')
-            return self.acs
-
         # Remove duplicate ultrametric ACs
         acs2 = []
         ults = _MySet()
@@ -225,15 +167,7 @@ class PHAC:
             U = _pUltrametric(ac, self.N)
             if ults.add(U):
                 acs2.append(ac)
-        self.acs = acs2
 
-        norm = lambda ac : _pUltrametric(ac, self.N).norm(self.ord)
-        acs  = sorted(self.acs, key=norm)
-        best = norm(acs[0])
-        i    = 1
-        while i < len(acs) and norm(acs[i]) == best:
-            i += 1
-
-        self.log.info('Returning %d bests with fitting error %1.3f', i, norm(acs[0]))
-
-        return acs[:i]
+        self.log.info('Returning %d unique solutions, having P=%1.3e',
+                      len(acs2), np.product(acs2[0].dists))
+        return acs2
--- test/ophac/phac_test.py
+++ test/ophac/phac_test.py
@@ -40,8 +40,34 @@ class PHAC_Test(ut.UnitTest):
         self.assertEquals(1, len(acs), 'Too many results')
 
         expJoins = [(0,3),(0,2),(0,1)]
-        expDists = [3/10, 3/7, 3/3]
+        expDists = [1-3/10, 1-3/7*3/10, 1-3/7*3/10]
 
         self.assertEquals(expJoins, acs[0].joins, 'Wrong joins')
         self.assertAllClose(expDists, acs[0].dists, msg='Wrong join probabilities')
         
+
+    def testBimodalDist(self):
+        '''
+        Distribution that has two disjoint (p=0) sets of elements
+        '''
+        p = [ 5.751e-02, 1.735e-02, 3.114e-02, 0.000e+00, 1.076e-02, 2.833e-02,
+              3.289e-02, 2.321e-02, 1.020e-02, 9.302e-03, 2.104e-02, 0.000e+00,
+              2.851e-02, 4.843e-02, 1.075e-02, 4.311e-02, 2.637e-02, 5.362e-02,
+              0.000e+00, 4.088e-02, 2.436e-02, 4.852e-02, 2.859e-02, 1.003e-02,
+              0.000e+00, 3.180e-02, 3.387e-02, 2.025e-02, 4.524e-02, 2.908e-02,
+              0.000e+00, 0.000e+00, 0.000e+00, 0.000e+00, 0.000e+00, 2.597e-02,
+              2.653e-02, 1.114e-02, 3.978e-02, 2.184e-02, 4.647e-02, 1.133e-03,
+              8.623e-03, 2.282e-02, 3.054e-02]
+        P0 = phac.DistMatrix(p)
+        hc = phac.PHAC('+')
+        ac = hc.generate(P0)
+        self.assertEquals(1, len(ac), 'more than one result')
+        ac = ac[0]
+
+        self.assertEquals(P0.n-1, len(ac), 'Too shallow hac.')
+        
+        for i in range(len(ac)-1):
+            self.assertTrue(ac.dists[i] <= ac.dists[i+1], 'Failed with %f >= %f for i=%d and i+1=%d' %
+                            (ac.dists[i], ac.dists[i+1], i, i+1))
+    
+    
